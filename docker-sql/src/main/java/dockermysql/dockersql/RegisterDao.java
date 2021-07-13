22d894d27b1b7fd3e5ccaea77c1962586572a32a
Rishi7pillai <hrushikeshpollai777@gmail.com>
2021-07-13 10:33:34 +0530

    second commit

--- docker-sql/src/main/java/dockermysql/dockersql/RegisterDao.java
+++
@@ -1,7 +0,0 @@
-package dockermysql.dockersql;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RegisterDao extends JpaRepository<Register,Long> {
-    Register findByuserId(Long userId);
-}
